d0a1c335865eb9a707cfb38f62bf311cc6df9893
Matthew Weier O'Phinney <matthew@zend.com>
2012-12-10 08:23:36 -0600

    Local modifications for vimes

--- diraliases.zsh
+++ diraliases.zsh
@@ -5,11 +5,14 @@ zend=/home/matthew/mydocs/zend/2012
 tmp=/home/matthew/tmp
 
 # Zend Framework
-zfweb=/home/matthew/git/zend-sites/zf-web
-zf2=/home/matthew/git/zf/zf2
+extras=/home/matthew/git/zf-extras
+quickstart=/home/matthew/git/zf-quickstart
+zfweb=/home/matthew/git/zf-web
+zf1=/home/matthew/git/zf-standard
+zf2=/home/matthew/git/zf2
 
 # Personal projects
 git=/home/matthew/git
 mwop=/home/matthew/git/sites/mwop
 
-: ~mydocs ~zend ~tmp ~zf2 ~zfweb ~git ~mwop
+: ~mydocs ~zend ~tmp ~extras ~quickstart ~zf1 ~zf2 ~zfweb ~git ~mwop
