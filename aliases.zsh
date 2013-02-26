fa93b4ff3efe32683aa054caf7c3ae29eb8fa76b
Matthew Weier O'Phinney <matthew@zend.com>
2013-02-26 15:41:21 -0600

    Updated git alias
    - well, function

--- aliases.zsh
+++ aliases.zsh
@@ -3,7 +3,7 @@ alias ack=ack-grep
 alias docblox="/usr/local/zend/bin/docblox"
 # alias gview="gview -f"
 # alias gvim="gvim -f"
-alias git="$HOME/bin/hub"
+# alias git="$HOME/bin/hub"
 alias ls="ls --color=auto"
 alias man="TERMINFO=~/.terminfo/ LESS=C TERM=mostlike PAGER=less man"
 alias mutt="/usr/bin/mutt -F ~/.muttrc -f ="
@@ -13,3 +13,18 @@ alias tmux="/usr/local/tmux-1.6/bin/tmux"
 alias vagrant="/opt/vagrant/bin/vagrant"
 # alias vim="/usr/bin/vim -X"
 alias zf="$HOME/bin/php $HOME/bin/zf.php"
+
+# Per https://github.com/robbyrussell/oh-my-zsh/issues/766#issuecomment-4267841
+git() {
+    if ! (( $+_has_working_hub  ))
+    then
+        hub --version &> /dev/null
+        _has_working_hub=$(($? == 0)) 
+    fi
+    if (( $_has_working_hub ))
+    then
+        hub "$@"
+    else
+        command git "$@"
+    fi
+}
